703d1c54a4b071f352326ddc85bdebfeb6cd8d9f
Lina Necib <lnecib@mit.edu>
2022-09-17 08:35:30 -0500

    added h5py and pygaia to the requirements since it did not work for me without them, also changed the allocation number and added a line that activated a virtual env, whcih should be edited

--- write_slurm.py
+++ write_slurm.py
@@ -61,23 +61,27 @@ run_cmd = "srun -n1 -N1 python make_catalog.py "\
     f"--cache-file {cache_file} --ext-var bminr --ext-extrapolate "\
     "--err-extrapolate --ijob 0 --Njob 1"
 
+# Activate the virtual env
+virtual_env = "source $HOME/ananke/bin/activate" + "\n"
+
 # Create sbatch file to submit make_catalog
 sbatch_fn = os.path.join(submit_dir, "make_catalog.sh")
 with open(sbatch_fn, 'w') as f:
     f.write('#!/bin/bash\n')
     f.write('#SBATCH -p {}\n'.format(partition))
-    f.write('#SBATCH -A TG-AST140023\n')
+    f.write('#SBATCH -A TG-PHY210118\n')
     f.write('#SBATCH --job-name {}\n'.format(name))
     f.write('#SBATCH --time {}\n'.format(t1))
     f.write('#SBATCH --mail-type begin\n')
     f.write('#SBATCH --mail-type end\n')
-    f.write('#SBATCH --mail-user tnguy@mit.edu\n')
+    f.write('#SBATCH --mail-user lnecib@mit.edu\n')
     f.write('#SBATCH -o make_catalog.out\n')
     f.write('#SBATCH -e make_catalog.err\n')
     f.write('#SBATCH --nodes=1\n')
     f.write('#SBATCH --ntasks=1\n')
     f.write('mkdir -p {}\n'.format(os.path.dirname(cache_file)))
     f.write('mkdir -p {}\n'.format(os.path.dirname(out_file)))
+    f.write(virtual_env)
     f.write('cd {}\n'.format(os.getcwd()))
     f.write(run_cmd + '\n')
     f.write('exit 0\n')
@@ -104,16 +108,17 @@ sbatch_fn = os.path.join(submit_dir, "selection_fn.sh")
 with open(sbatch_fn, 'w') as f:
     f.write('#!/bin/bash\n')
     f.write('#SBATCH -p {}\n'.format(partition))
-    f.write('#SBATCH -A TG-AST140023\n')
+    f.write('#SBATCH -A TG-PHY210118\n')
     f.write('#SBATCH --job-name {}\n'.format(name))
     f.write('#SBATCH --time {}\n'.format(t2))
     f.write('#SBATCH --mail-type begin\n')
     f.write('#SBATCH --mail-type end\n')
-    f.write('#SBATCH --mail-user tnguy@mit.edu\n')
+    f.write('#SBATCH --mail-user lnecib@mit.edu\n')
     f.write('#SBATCH -o selection_fn.out\n')
     f.write('#SBATCH -e selection_fn.err\n')
     f.write('#SBATCH --nodes=1\n')
     f.write('#SBATCH --ntasks=1\n')
+    f.write(virtual_env) 
     f.write('cd {}\n'.format(os.getcwd()))
     f.write(run_cmd + '\n')
     f.write('exit 0\n')
